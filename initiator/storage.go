a0763c6bc1225aed5cd99a224bf0da9baa6fbab1
Nebil Muhidin <nebilmuhidin57@gmail.com>
2024-05-31 10:50:51 +0300

    add the sqlc yaml file

---
+++ initiator/storage.go
@@ -0,0 +1,5 @@
+package initiator
+
+func InitPersistence() {
+
+}
--- utils/logger/logger.go
+++ utils/logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"context"
+	"time"
 
 	"go.uber.org/zap"
 )
@@ -10,6 +11,8 @@ type Logger interface {
 	Error(ctx context.Context, msg string, fields ...zap.Field)
 	Info(ctx context.Context, msg string, fields ...zap.Field)
 	Fatal(ctx context.Context, msg string, fields ...zap.Field)
+	With(fields ...zap.Field) *zap.Logger
+	extract(ctx context.Context) []zap.Field
 }
 
 type logger struct {
@@ -23,13 +26,35 @@ func NewLogger(log *zap.Logger) Logger {
 }
 
 func (l *logger) Error(ctx context.Context, msg string, fields ...zap.Field) {
-
+	l.With(l.extract(ctx)...).Error(msg, fields...)
+	// l.logger.With(l.extract(ctx)...).Error(msg,fields...)
 }
 
 func (l *logger) Info(ctx context.Context, msg string, fields ...zap.Field) {
-
+	l.With(l.extract(ctx)...).Info(msg, fields...)
 }
 
 func (l *logger) Fatal(ctx context.Context, msg string, fields ...zap.Field) {
+	l.With(l.extract(ctx)...).Fatal(msg, fields...)
+}
 
+func (l *logger) With(fields ...zap.Field) *zap.Logger {
+	l2 := l.logger.With(fields...)
+	return l2
+}
+
+func (l *logger) extract(ctx context.Context) []zap.Field {
+	var fields []zap.Field
+
+	fields = append(fields, zap.String("time", time.Now().Format(time.RFC3339)))
+
+	if reqID, ok := ctx.Value("x-request-id").(string); ok {
+		fields = append(fields, zap.String("x-request-id", reqID))
+	}
+
+	if hitTime, ok := ctx.Value("time-since-start").(time.Time); ok {
+		fields = append(fields, zap.Float64("time-since-start", float64(time.Since(hitTime).Milliseconds())))
+	}
+
+	return fields
 }
